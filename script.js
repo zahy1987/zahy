b04ab42fd032698083b973c2bbda574a4370523f
zahy1987 <shopzahy@gmail.com>
2025-10-08 07:14:04 +0100

    Update script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+<script>
+  const menuToggle = document.getElementById('menu-toggle');
+  const menuList = document.getElementById('menu-list');
+
+  menuToggle.addEventListener('click', () => {
+    menuList.classList.toggle('show');
+  });
+</script>
