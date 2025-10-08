93febeee097de17d7a79171a1306e16669f64060
zahy1987 <shopzahy@gmail.com>
2025-10-08 07:54:34 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,7 @@
 alert.log('hy wold')
+console.log('Hi world');
+// Exemplu JS pentru hamburger:
+document.querySelector('.menu-toggle').onclick = function() {
+  document.querySelector('.menubar ul').classList.toggle('open');
+};
 
