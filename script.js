5b3ed763febb745943d13648361ede60599f6999
zahy1987 <shopzahy@gmail.com>
2025-10-09 07:39:31 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+console.log('Hi world');
+// Exemplu JS pentru hamburger:
+document.querySelector('.menu-toggle').onclick = function() {
+  document.querySelector('.menubar ul').classList.toggle('open');
+};
+const slides = document.querySelectorAll('.item:nth-child(2) .slide');
+let index = 0;
 
+setInterval(() => {
+  slides[index].classList.remove('active');
+  index = (index + 1) % slides.length;
+  slides[index].classList.add('active');
+}, 2000); // la fiecare 5 secunde
