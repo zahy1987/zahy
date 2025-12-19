1d1242d32df761a1af958160dc7fc095eb184148
Gabriel Alexandru Zaharia <shopzahy@gmail.com>
2025-12-19 10:10:04 +0000

    Add files via upload

---
+++ My site/Proiecte/Login buton/script.js
@@ -0,0 +1,17 @@
+// Step 1: Create a userName variable with your name
+let userName = "Gabriel"; // Fill in your name here as a string
+
+// Step 2: Create an age variable with a number
+var age = 39;{
+
+} // Fill in your age here as a number
+
+
+// Step 3: Use console.log to output a message with your name and age
+console.warn("My name is " + userName + " and I am " + age + " years old.");
+
+// Step 4: Create a future variable that adds 10 to your age
+let future = age + 10; // Write the expression to calculate your age in 10 years
+
+// Step 5: Use console.log to output a message with your future age
+console.error("In 10 years, I will be " + future + " years old.");
