188d4a814a0ed06eefa7c87c7f6f90d16a7a3b45
Gabriel Alexandru Zaharia <shopzahy@gmail.com>
2025-12-28 15:24:58 +0000

    Add files via upload

--- My site/home/script.js
+++ My site/home/script.js
@@ -1,14 +1,13 @@
-/* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
-    function myFunction() {
-      var x = document.getElementById("myLinks");
-      if (x.style.display === "block") {
-        x.style.display = "none";
-      } else {
-        x.style.display = "block";
-      }
-    }
-
-
+// Funcție pentru hamburger menu
+function myFunction() {
+  var topnav = document.querySelector(".topnav");
+  
+  if (topnav.classList.contains("responsive")) {
+    topnav.classList.remove("responsive");
+  } else {
+    topnav.classList.add("responsive");
+  }
+}
     /* snow home */
 
       document.addEventListener('DOMContentLoaded', function() {
