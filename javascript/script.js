6891db04ce94e45294be9be602db4977dfbc1736
Gabriel Alexandru Zaharia <shopzahy@gmail.com>
2026-01-14 12:04:21 +0000

    Add files via upload

--- javascript/script.js
+++ javascript/script.js
@@ -1,78 +1,129 @@
 //Variables declared with var, let , and const
-var thisIsVar ="This is a var variabke"; //var variable
-let letVariable = [1, 2, 3,]; // const varable
+var thisIsVar = "This is a var variable"; // var variable
+let letVariable = [1, 2, 3]; // let variable
+const constVariable = [10, 20, 30]; // const variable
 
-// Named fuction
-function learning() {  //named fuction
+
+/*------------------------------------------
+FUNCȚII
+- asta e o funcție cu nume
+- codul din interior nu se execută singur
+- doar când apeși numele funcției, rulează
+------------------------------------------*/
+function learning() {  //named function
     console.log("This is a named function.");
 }
 
+//apelezi funcția aici
 learning();
 
-//Anonimous fuction assigned to a variable
+
+/*------------------------------------------
+FUNCȚIE ANONIMĂ
+- nu are nume, e stocată într-o variabilă
+- variabila devine „butonul” care pornește codul
+------------------------------------------*/
 let myDreamsRemote = function() {
-    console.log("This is an anonymous dream fuction.");
+    console.log("This is a anonymous dream function.");
 };
 
+//apelezi funcția
 myDreamsRemote();
 
-// Built-in fuction  (exemple: preseInt)
+
+/*------------------------------------------
+BUILT-IN FUNCTION
+- JavaScript are deja funcții gata făcute
+- parseInt transformă text în număr
+------------------------------------------*/
 let numberString = "123";
 let parsedNumber = parseInt(numberString);
 console.log("Parsed number using parseInt:", parsedNumber);
 
-// if/else conditional statement
+
+/*------------------------------------------
+IF / ELSE
+- verifică o condiție și alege ce să facă
+- true = dacă e adevărat, else = dacă e fals
+------------------------------------------*/
 let condition = true;
+
 if (condition) {
     console.log("Condition is true.");
-}
-else {
-    console.log("condition is false.");
+} else {
+    console.log("Condition is false.");
 }
 
-//Switch conditional statement
+
+/*------------------------------------------
+SWITCH
+- când ai mai multe opțiuni și vrei să alegi una
+- default = dacă nu se potrivește niciun caz
+------------------------------------------*/
 let day = 3;
+
 switch (day) {
     case 1:
         console.log("Monday");
         break;
-    case 1:
+    case 2:
         console.log("Tuesday");
         break;
-    case 1:
+    case 3:
         console.log("Wednesday");
         break;
-    case 1:
+    case 4:
         console.log("Thursday");
         break;
-    case 1:
+    case 5:
         console.log("Friday");
         break;
-        default:
-            console.log("Weekend");
+    default:
+        console.log("Weekend");
 }
 
-//Array of different data types
-let stringArray = ["apple" , "banana" , "cherry"]; //Array of strings
-let numberArray = [1, 2, 3, 4, 5, 66, 999]; //array of numbers
-let mixedArray = [ 1, 30, "bla", true];
-console.log("bla bla bla", mixedArray);
 
-// Loop through an array
-for ( let i =0; i < numberArray.length; i++) {
-        console.log("String array element:", numberArray[i]);
+/*------------------------------------------
+ARRAY-URI
+- liste de chestii
+- pot fi doar text, doar numere sau mix
+------------------------------------------*/
+let stringArray = ["apple", "banana", "cherry"]; // array of strings
+let numberArray = [1, 2, 3, 4, 5, 66, 999]; // array of numbers
+let mixedArray = [1, 30, "bla", true]; // mixed data types
+
+console.log("Mixed array:", mixedArray);
+
+
+/*------------------------------------------
+FOR LOOP
+- rulează codul pentru fiecare element din listă
+- i începe de la 0 și merge până la lungimea array-ului
+------------------------------------------*/
+for (let i = 0; i < numberArray.length; i++) {
+    console.log("Number array element:", numberArray[i]);
+}
+
+
+/*------------------------------------------
+OBIECTE
+- grup de chestii: proprietăți + funcții
+- this = se referă la obiectul curent
+------------------------------------------*/
+let person = {
+    firstName: "zahy",
+    lastName: "yhaz",
+    age: 38,
+    fullName: function() {
+        // returnează numele complet
+        return this.firstName + " " + this.lastName;
     }
+};
 
-    // Creating and using objects
-    let person = {
-        firstname: "zahy",
-        lastName: "yhaz",
-        age: 38,
-        fullname: function() {
-            return this.firstname + "" + this.lastName;
-        }
-    };
 
-    // Accessing object properties
-    console.log(`Person"s first name:`, person.firstname); //Access firstName property
-    console.log("Person`s full name:", person.fullname()); // call fullName method
+/*------------------------------------------
+ACCESAREA PROPRIETĂȚILOR OBIECTULUI
+- pun console.log ca să văd ce e în interior
+------------------------------------------*/
+console.log(`Person's first name:`, person.firstName); // Access firstName property
+console.log("Person`s full name:", person.fullName()); // Call fullName method
