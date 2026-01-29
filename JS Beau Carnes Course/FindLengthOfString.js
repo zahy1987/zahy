8e5c9dcbcfc9da654cfbe60ec27df06edc83c7e8
Gabriel Alexandru Zaharia <shopzahy@gmail.com>
2026-01-29 22:55:22 +0000

    Find Length Of String

---
+++ JS Beau Carnes Course/FindLengthOfString.js
@@ -0,0 +1,29 @@
+// Gasiti lungimea sirului de caractere
+
+// Declar o variabilă inițializată cu 0 care va stoca lungimea numelui
+var firstNameLength = 0;
+
+// Definim un string care reprezintă prenumele
+var firstName = "yhaz";
+
+// Atribuim variabilei firstNameLength lungimea string-ului firstName folosind proprietatea .length
+firstNameLength = firstName.length;
+
+// Afișăm în consolă lungimea prenumelui
+console.log(firstNameLength); // Va afișa 4 (deoarece "yhaz" are 4 caractere)
+
+// setup (configurare inițială pentru a lucra cu numele de familie)
+
+// Declar o variabilă inițializată cu 0 care va stoca lungimea numelui de familie
+var lastNameLength = 0;
+
+// Definim un string care reprezintă numele de familie
+var lastName = "Lovelace";
+
+// Only change code below this line (Modifică doar codul de mai jos)
+
+// Atribuim variabilei lastNameLength lungimea string-ului lastName folosind proprietatea .length
+lastNameLength = lastName.length;
+
+// Afișăm în consolă lungimea numelui de familie
+console.log(lastNameLength); // Va afișa 8 (deoarece "Lovelace" are 8 caractere)
