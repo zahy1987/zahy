4a3f61f48f76d9078208b30609d16c09a89808a4
Gabriel Alexandru Zaharia <shopzahy@gmail.com>
2026-01-29 22:01:34 +0000

    concatenate strings

---
+++ JS Beau Carnes Course/string-concatenation.js
@@ -0,0 +1,11 @@
+var ourName = "zahy"
+var ourStr = "Hello, our name is " + ourName + ", how are you?";
+console.log(ourStr);
+
+var myName = "Gabriel"; // definit variabila myName cu valoarea  " Gabriel"
+var myStr = "My name is " + myName + " and I am well!"; // folosit concatenarea pentru a crea myStr, care va fi:"My name is Gabriel and I am well!"
+console.log(myStr);
+
+/* asta este clasicu
+ cum sa legi mai multe siruri (string)
+  */
