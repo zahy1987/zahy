08129b6afac38b8dce24ba25d9b7ef5a71fd1043
Gabriel Alexandru Zaharia <shopzahy@gmail.com>
2026-01-30 01:14:47 +0000

    Braket Notation Find  Caracter String
    []

---
+++ JS Beau Carnes Course/braketNotationFinCaracterString.js
@@ -0,0 +1,13 @@
+// EXERCIȚIU: Accesarea primului caracter dintr-un string
+
+// Exemplul 1 - Prenume
+var firstLetter0fFirstName = "";  // Variabilă pentru primul caracter
+var firstName = "yhaz";            // String-ul sursă
+firstLetter0fFirstName = firstName[0]; // Accesare caracter la index 0
+console.log(firstLetter0fFirstName);   // Afișare: "y"
+
+// Exemplul 2 - Nume de familie
+var firstLetter0fLasttName = "";  // Variabilă pentru primul caracter
+var lastName = "Lovelace";         // String-ul sursă
+firstLetter0fLasttName = lastName[0]; // Accesare caracter la index 0
+console.log(firstLetter0fLasttName);   // Afișare: "L"
