7ad72fa437db352373858cb3c446cfd844dc5e89
Gabriel Alexandru Zaharia <shopzahy@gmail.com>
2026-01-30 21:40:37 +0000

    Accessing characters from a string in JavaScript (with Bracket Notation)
    I am practicing:
    
    how to get the first character from a string (index 0)
    
    how to get the last character length - 1
    
    how to get characters from the end, such as the second or third from the end (length - 2, length - 3)
    
    These techniques are essential for working with text, data validation, and string manipulation in JavaScript.

--- JS ITonlinelarning & Beau Carnes Course/braketNotationFinCaracterString.js
+++ JS ITonlinelarning & Beau Carnes Course/braketNotationFinCaracterString.js
@@ -11,3 +11,34 @@ var firstLetter0fLasttName = "";  // Variabilă pentru primul caracter
 var lastName = "Lovelace";         // String-ul sursă
 firstLetter0fLasttName = lastName[0]; // Accesare caracter la index 0
 console.log(firstLetter0fLasttName);   // Afișare: "L"
+
+//________________________________________________________________________________________________________
+
+
+// Bracket Notation to find last character in string
+
+var firstName ="Ada";
+var lastLetterOfFirstName = firstName[firstLetter0fFirstName.length - 1];
+
+//setup
+
+var lastName = "Lovelace";
+
+// Only change code below this line.
+
+var lastLetterOfFirstName = lastName[lastName.length -1];
+
+//_____________________________________________________________________________________________________
+
+// Braket Notation to find  Nth-to-Last caracter in string
+
+var firstName = "Ada";
+var thirdToLastLetterOfFirstName = firstName[firstName.length -3];
+console.log(firstName);
+
+//Setup
+Var = lastName = "Lovelace";
+
+// Only change code below this line
+var secondToLastLetterOfLastName = lastName[lastName.length -2];
+console.log(secondToLastLetterOfLastName);
