e041c50cb8ed4e09c5b64a44ed774321a09a80d1
Gabriel Alexandru Zaharia <shopzahy@gmail.com>
2026-01-30 23:52:20 +0000

    update later

---
+++ JS ITonlinelarning & Beau Carnes Course/storeMultipleValuteWithArrays.js
@@ -0,0 +1,26 @@
+var ourArray = ["zahy", 23];
+
+// Only change code below this line.
+var myArray = ["Quincy", 1];
+
+
+//Nested Arrays
+
+var ourArray = [["the universe", 42], ["everything", 101010]];
+
+// Only change code below this line
+var myArray = [["Bulls", 23], ["White Sox", 43 ]];
+
+
+// Access Array Data with Indexes
+
+var ourArray = [50,60,70];
+var ourData = ourArray[1]; // equals 60
+console.log(ourData);
+
+//Setup
+var myArray = [50,60,70];
+
+//Only change code below th8s line.
+var myData = myArray[2]; // equals 70
+console.log(myData);
